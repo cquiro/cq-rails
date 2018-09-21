58f8a3483d1f1f354b369f8ee9055d3070be9074
Carlos Quiroga <cquiro@gmail.com>
2018-09-21 10:33:14 -0300

    Install and configure devise_token_auth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  mount_devise_token_auth_for 'User', at: 'users'
+
   devise_scope :user do
     get '/', to: "devise/registrations#new"
   end
