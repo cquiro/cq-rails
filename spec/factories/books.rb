bf42a12e69239b1b1e753dcb379e46c835d9a442
Carlos Quiroga <cquiro@gmail.com>
2018-09-21 17:01:41 -0300

    Add tests to the book model
    - Create a Book factory.
    
    - Write specs to test book validations.

---
+++ spec/factories/books.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :book do
+    genre { Faker::Book.genre }
+    author { Faker::Book.author }
+    image { Faker::Placeholdit.image("350x500") }
+    title { Faker::Book.title }
+    publisher { Faker::Book.publisher }
+    year { Faker::Number.between(1450, 2018).to_s }
+  end
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+describe Book do
+  it "is valid with valid attributes" do
+    book = build(:book)
+
+    expect(book).to be_valid
+  end
+
+  it "is not valid without a genre" do
+    book = build(:book, genre: nil)
+
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without an author" do
+    book = build(:book, author: nil)
+
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without an image" do
+    book = build(:book, image: nil)
+
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without a title" do
+    book = build(:book, title: nil)
+
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without a publisher" do
+    book = build(:book, publisher: nil)
+
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without a year" do
+    book = build(:book, year: nil)
+
+    expect(book).not_to be_valid
+  end
+end
