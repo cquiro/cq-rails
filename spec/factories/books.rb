33db5ca26c64d346a577fcb585b67561286d8d21
Carlos Quiroga <cquiro@gmail.com>
2018-09-21 17:35:29 -0300

    Fix rubocop offenses
    - Exclude spec_helper and rails helper from all cops.
    
    - Exclude all files under spec from Metrics/BlockLength cop.

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :book do
     genre { Faker::Book.genre }
     author { Faker::Book.author }
-    image { Faker::Placeholdit.image("350x500") }
+    image { Faker::Placeholdit.image('350x500') }
     title { Faker::Book.title }
     publisher { Faker::Book.publisher }
     year { Faker::Number.between(1450, 2018).to_s }
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,43 +1,43 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe Book do
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     book = build(:book)
 
     expect(book).to be_valid
   end
 
-  it "is not valid without a genre" do
+  it 'is not valid without a genre' do
     book = build(:book, genre: nil)
 
     expect(book).not_to be_valid
   end
 
-  it "is not valid without an author" do
+  it 'is not valid without an author' do
     book = build(:book, author: nil)
 
     expect(book).not_to be_valid
   end
 
-  it "is not valid without an image" do
+  it 'is not valid without an image' do
     book = build(:book, image: nil)
 
     expect(book).not_to be_valid
   end
 
-  it "is not valid without a title" do
+  it 'is not valid without a title' do
     book = build(:book, title: nil)
 
     expect(book).not_to be_valid
   end
 
-  it "is not valid without a publisher" do
+  it 'is not valid without a publisher' do
     book = build(:book, publisher: nil)
 
     expect(book).not_to be_valid
   end
 
-  it "is not valid without a year" do
+  it 'is not valid without a year' do
     book = build(:book, year: nil)
 
     expect(book).not_to be_valid
