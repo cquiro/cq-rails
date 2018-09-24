9e77a123d07ade175c03cdfe14d03fc1a85ed07e
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 18:12:44 -0300

    Create initial code for books endpoint
    - Add route for api/v1/books
    
    - Add index action to BooksController and return Book.all
    
    - No pagination or filters implemented yet

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      def index
+        render json: books, each_serializer: Api::V1::BookSerializer, status: :ok
+      end
+
       def show
         render json: book, serializer: Api::V1::BookSerializer, status: :ok
       end
@@ -9,6 +13,10 @@ module Api
 
       def book
         @book ||= Book.find(params[:id])
+      end
+
+      def books
+        @books ||= Book.all
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'users'
 
   api_version(module: 'Api::V1', path: { value: 'api/v1' }, defaults: { format: :json })  do
-    resources :books, only: [:show]
+    resources :books, only: [:index, :show]
   end
 end
