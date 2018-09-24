affdcae82ac0777e5a46b069de5d7528b616a5e8
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 16:12:28 -0300

    Test custom validation for success
    In rent_spec.rb test if custom validation works, not if the error
    message returned is as expected.

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 describe Rent do
-  subject do
-    build(:rent)
+  context 'with built-in validations' do
+    subject { build(:rent) }
+
+    it { is_expected.to be_valid }
+
+    it { is_expected.to validate_presence_of(:start_date) }
+
+    it { is_expected.to validate_presence_of(:end_date) }
   end
 
-  it { is_expected.to be_valid }
+  context 'with custom validations' do
+    subject { build(:rent, start_date: Time.current, end_date: 2.days.ago) }
 
-  it { is_expected.to validate_presence_of(:start_date) }
-
-  it { is_expected.to validate_presence_of(:end_date) }
-
-  it 'is not valid when end_date < start_date' do
-    rent = build(:rent, start_date: Time.current, end_date: 2.days.ago)
-    rent.valid?
-
-    expect(rent.errors[:end_date]).to include 'can\'t be earlier than start date'
+    it { is_expected.not_to be_valid }
   end
 end
