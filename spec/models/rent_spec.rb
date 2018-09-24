873142f6b4376b72adc6067698bd82ddda00a45e
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 11:28:05 -0300

    Add specs for rent
    - Write specs for rent
    - Write validator in rent to make sure end_date is never earlier than
    start date.

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe Rent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Rent do
+  it 'is valid with valid attributes' do
+    rent = build(:rent)
+
+    expect(rent).to be_valid
+  end
+
+  it 'is not valid without a user' do
+    rent = build(:rent, user: nil)
+
+    expect(rent).not_to be_valid
+  end
+
+  it 'is not valid without a book' do
+    rent = build(:rent, book: nil)
+
+    expect(rent).not_to be_valid
+  end
+
+  it 'is not valid without a start_date' do
+    rent = build(:rent, start_date: nil)
+
+    expect(rent).not_to be_valid
+  end
+
+  it 'is not valid without an end_date' do
+    rent = build(:rent, end_date: nil)
+
+    expect(rent).not_to be_valid
+  end
+
+  it 'is not valid when end_date < start_date' do
+    rent = build(:rent, start_date: Time.current, end_date: 2.days.ago)
+    rent.valid?
+
+    expect(rent.errors[:end_date]).to include 'can\'t be earlier than start date'
+  end
 end
