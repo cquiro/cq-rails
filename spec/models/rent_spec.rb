e23970cae7950e6af63132aefbd30dac560e0a4b
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 14:42:45 -0300

    Use one-liners to test validations in rent_spec.rb

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,35 +1,15 @@
 require 'rails_helper'
 
 describe Rent do
-  it 'is valid with valid attributes' do
-    rent = build(:rent)
-
-    expect(rent).to be_valid
+  subject do
+    build(:rent)
   end
 
-  it 'is not valid without a user' do
-    rent = build(:rent, user: nil)
+  it { is_expected.to be_valid }
 
-    expect(rent).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of(:start_date) }
 
-  it 'is not valid without a book' do
-    rent = build(:rent, book: nil)
-
-    expect(rent).not_to be_valid
-  end
-
-  it 'is not valid without a start_date' do
-    rent = build(:rent, start_date: nil)
-
-    expect(rent).not_to be_valid
-  end
-
-  it 'is not valid without an end_date' do
-    rent = build(:rent, end_date: nil)
-
-    expect(rent).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of(:end_date) }
 
   it 'is not valid when end_date < start_date' do
     rent = build(:rent, start_date: Time.current, end_date: 2.days.ago)
