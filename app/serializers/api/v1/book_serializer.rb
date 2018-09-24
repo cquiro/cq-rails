94dda65855b008ef1fb13829deec285938f23a16
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 17:50:00 -0300

    Return the image url directly.

--- app/serializers/api/v1/book_serializer.rb
+++ app/serializers/api/v1/book_serializer.rb
@@ -3,10 +3,6 @@ module Api
     class BookSerializer < ActiveModel::Serializer
       attributes :id, :author, :title, :image, :publisher, :year, :genre,
                  :created_at, :updated_at
-
-      def image
-        { url: object.image }
-      end
     end
   end
 end
