984b59c2da3836bedb0115e3b8a09cc04a55f86d
Carlos Quiroga <cquiro@gmail.com>
2018-09-24 10:36:51 -0300

    Fix rubocop offenses
    - Add dependent: :destroy to has_many line in book and user.
    - Replace double quotes with single quotes in rents and users factories.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :rents
+  has_many :rents, dependent: :destroy
 end
--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rent do
     user
     book
-    start_date { "2018-09-24" }
-    end_date { "2018-09-24" }
+    start_date { '2018-09-24' }
+    end_date { '2018-09-24' }
   end
 end
