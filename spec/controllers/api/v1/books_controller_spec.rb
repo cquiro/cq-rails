d7209266bd411ebe84f9f4a92b17440c7174543d
Carlos Quiroga <cquiro@gmail.com>
2018-09-25 15:40:42 -0300

    Write BooksController specs
    - Write passing specs for BooksController#show
    
    - Write failing specs for BooksController#index
    
    - Add devise controller helpers in rails_helper.rb

---
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe Api::V1::BooksController do
+  include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    let!(:books) { create_list(:book, 3) }
+
+    before do
+      get :index
+    end
+
+    it { expect(response).to have_http_status(:ok) }
+
+    it 'responds with the books list in json format' do
+      expected = ActiveModel::Serializer::CollectionSerializer.new(
+        books, each_serializer: Api::V1::BookSerializer, root: false
+      ).to_json
+      expect(response.body.to_json) =~ JSON.parse(expected)
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:book) { create(:book) }
+
+    before do
+      get :show, params: { id: book.id }
+    end
+
+    it { expect(response).to have_http_status(:ok) }
+
+    it 'responds with the book in json format' do
+      expect(response.body).to eq Api::V1::BookSerializer.new(
+        book, root: false
+      ).to_json
+    end
+
+  end
+end
