e0159d34ee71c5b811930e3d5c51d684381bfcfc
Carlos Quiroga <cquiro@gmail.com>
2018-09-26 16:23:11 -0300

    Add specs for index and create actions in RentsController.

---
+++ spec/controllers/api/v1/rents_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe Api::V1::RentsController do
+  include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    let!(:rents) { create_list(:rent, 3, user: user) }
+
+    before { get :index, params: { user_id: user.id } }
+
+    it { expect(response).to have_http_status(:ok) }
+
+    it 'responds with the user\'s rents in json format' do
+      expected = ActiveModel::Serializer::CollectionSerializer.new(
+        rents, each_serializer: RentSerializer, root: false
+      ).to_json
+
+      expect(response_body.to_json) =~ JSON.parse(expected)
+    end
+  end
+
+  describe 'POST #create' do
+    let!(:book) { create(:book) }
+    let!(:rent_attrs) { attributes_for(:rent) }
+
+    before do
+      post :create, params: {
+        user_id: user.id,
+        rent: rent_attrs.merge(book_id: book.id)
+      }
+    end
+
+    it { expect(response).to have_http_status(:created) }
+
+    it 'responds with the serialized new rent' do
+      expect(response_body['start_date'].to_date).to eq rent_attrs[:start_date]
+      expect(response_body['book']['id']).to eq book.id
+      expect(response_body['user']['id']).to eq user.id
+    end
+  end
+end
