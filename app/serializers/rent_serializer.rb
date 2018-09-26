4b583748c734825ddb7b0cb7b04ad3910b46badd
Carlos Quiroga <cquiro@gmail.com>
2018-09-26 14:54:31 -0300

    Return start_date and end_date instead of from and to in RentSerializer.

--- app/serializers/rent_serializer.rb
+++ app/serializers/rent_serializer.rb
@@ -1,14 +1,6 @@
 class RentSerializer < ActiveModel::Serializer
-  attributes :id, :from, :to, :returned_at
+  attributes :id, :start_date, :end_date, :returned_at
 
   has_one :book, serializer: BookSerializer
   has_one :user, serializer: UserSerializer
-
-  def from
-    object.start_date
-  end
-
-  def to
-    object.end_date
-  end
 end
