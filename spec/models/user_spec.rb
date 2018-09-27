937650d0d536bfd98697ea517d8e4e227b87c9f9
Carlos Quiroga <cquiro@gmail.com>
2018-09-27 14:05:54 -0300

    Validate rents_count >= 0 in the User model
    - Add validation to ensure rents_count is always >= 0
    
    - Create user_spec.rb and test new validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User do
+  subject do
+    build(:user)
+  end
+
+  it { is_expected.to be_valid }
+
+  it { is_expected.to validate_numericality_of(:rents_count).is_greater_than_or_equal_to(0) }
+end
