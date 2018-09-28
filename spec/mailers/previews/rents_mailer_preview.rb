cb75f18ef8f4933c49fa84647274d1c985443bc7
Carlos Quiroga <cquiro@gmail.com>
2018-09-28 15:52:23 -0300

    Fix rubocop offense

--- spec/mailers/previews/rents_mailer_preview.rb
+++ spec/mailers/previews/rents_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/rents_mailer
 class RentsMailerPreview < ActionMailer::Preview
   def send_new_rent_email
-    rent = Rent.first 
+    rent = Rent.first
 
     RentsMailer.send_new_rent_email(rent.id)
   end
