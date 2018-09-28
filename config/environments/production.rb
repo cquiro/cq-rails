c4299931ccdbc0f83e236493c6c6f43a9a423e9d
Carlos Quiroga <cquiro@gmail.com>
2018-09-28 10:35:00 -0300

    Configure mailtrap
    - Add mailtrap credentials to config/credentials.yml.enc
    
    - Set action_mailer.delivery_method to :smtp and configure mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials.mailtrap[:username],
+    :password => Rails.application.credentials.mailtrap[:password],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
