15af57dd14faee88bf57577db4413d84c6658d86
Carlos Quiroga <cquiro@gmail.com>
2018-10-01 10:41:24 -0300

    Set locale before sending email in RentsMailer.send_new_rent_email

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -3,7 +3,14 @@ class RentsMailer < ApplicationMailer
     @rent = Rent.find(rent_id)
     @user = @rent.user
     @book = @rent.book
+    set_locale
 
     mail(to: @user.email, subject: t('rents_mailer.new_rent.subject'))
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = @user.locale || I18n.default_locale
   end
 end
