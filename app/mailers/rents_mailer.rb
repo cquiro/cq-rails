f1f2381f85f451540d5b902093217692404a63b8
Carlos Quiroga <cquiro@gmail.com>
2018-10-03 15:51:45 -0300

    Add send_rent_due_email method to RentsMailer
    Add the necessary code so that an email can be sent
    to the user advising them to return a rental book on
    the day the rental is due.
    
    The send_rent_due_email method was added to the
    existing RentsMailer class.

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -1,14 +1,25 @@
 class RentsMailer < ApplicationMailer
   def send_new_rent_email(rent_id)
-    @rent = Rent.find(rent_id)
-    @user = @rent.user
-    @book = @rent.book
+    set_rental_data(rent_id)
     set_locale
 
     mail(to: @user.email, subject: t('rents_mailer.new_rent.subject'))
   end
 
+  def send_rent_due_email(rent_id)
+    set_rental_data(rent_id)
+    set_locale
+
+    mail(to: @user.email, subject: t('rents_mailer.rent_due.subject'))
+  end
+
   private
+
+  def set_rental_data(rent_id)
+    @rent = Rent.find(rent_id)
+    @user = @rent.user
+    @book = @rent.book
+  end
 
   def set_locale
     I18n.locale = @user.locale || I18n.default_locale
