1449c7001df9faed10dd542a60f89df385e9f224
Carlos Quiroga <cquiro@gmail.com>
2018-10-03 17:08:53 -0300

    Fix rubocop offenses

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -1,13 +1,13 @@
 class RentsMailer < ApplicationMailer
   def send_new_rent_email(rent_id)
-    set_rental_data(rent_id)
+    get_rental_data(rent_id)
     set_locale
 
     mail(to: @user.email, subject: t('rents_mailer.new_rent.subject'))
   end
 
   def send_rent_due_email(rent_id)
-    set_rental_data(rent_id)
+    get_rental_data(rent_id)
     set_locale
 
     mail(to: @user.email, subject: t('rents_mailer.rent_due.subject'))
@@ -15,7 +15,7 @@ class RentsMailer < ApplicationMailer
 
   private
 
-  def set_rental_data(rent_id)
+  def get_rental_data(rent_id)
     @rent = Rent.find(rent_id)
     @user = @rent.user
     @book = @rent.book
