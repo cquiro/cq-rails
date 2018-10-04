9f81a11972d93b39863844dd51f6a700c8c291c8
Carlos Quiroga <cquiro@gmail.com>
2018-10-04 17:10:55 -0300

    Replace method names that start with set or get
    Replace method names that start with set or get in RentsMailer as per
    rubocop-hq/ruby-style-guide.

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -1,27 +1,27 @@
 class RentsMailer < ApplicationMailer
   def send_new_rent_email(rent_id)
-    get_rental_data(rent_id)
-    set_locale
+    assign_rental_data(rent_id)
+    assign_locale
 
     mail(to: @user.email, subject: t('rents_mailer.new_rent.subject'))
   end
 
   def send_rent_due_email(rent_id)
-    get_rental_data(rent_id)
-    set_locale
+    assign_rental_data(rent_id)
+    assign_locale
 
     mail(to: @user.email, subject: t('rents_mailer.rent_due.subject'))
   end
 
   private
 
-  def get_rental_data(rent_id)
+  def assign_rental_data(rent_id)
     @rent = Rent.find(rent_id)
     @user = @rent.user
     @book = @rent.book
   end
 
-  def set_locale
+  def assign_locale
     I18n.locale = @user.locale || I18n.default_locale
   end
 end
