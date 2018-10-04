781aa42b82a706f08371f53ee88bf0ee5392eb55
Carlos Quiroga <cquiro@gmail.com>
2018-10-04 10:31:58 -0300

    Update code in RentDueNotifier
    During development some changes where made before commiting and I forgot
    to remove some code that was no longer necessary. In this commit that
    code is removed.

--- app/workers/rent_due_notifier.rb
+++ app/workers/rent_due_notifier.rb
@@ -2,14 +2,14 @@ class RentDueNotifier
   include Sidekiq::Worker
 
   def perform
-    Rent.where(id: rent_ids).find_each do |rent|
+    rents.find_each do |rent|
       RentsMailer.send_rent_due_email(rent.id).deliver_later
     end
   end
 
   private
 
-  def rent_ids
+  def rents
     Rent.where(end_date: Date.current.beginning_of_day..Date.current.end_of_day)
   end
 end
